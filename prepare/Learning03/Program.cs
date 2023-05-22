631b7a37a019cfb7636a3e9e3d9b0b7ef69e7039
alexedozi <chukwujioke60@gmail.com>
2023-05-22 17:12:43 +0200

    journal

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,23 +1,61 @@
 using System;
 
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
+    private int _top = 0;
+    private int _bottom = 0;
+
+    // Constructor with no parameters initializes to 1/1
+    public Fraction()
     {
-        Fraction f1 = new Fraction();
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimalValue());
-
-        Fraction f2 = new Fraction(5);
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimalValue());
-
-        Fraction f3 = new Fraction(3, 4);
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimalValue());
-
-        Fraction f4 = new Fraction(1, 3);
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimalValue());
+        _top = 1;
+        _bottom = 1;
     }
+    // Constructor with one parameters for top and initializes bottom to 1
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    // Constructor with two parameters for top and bottom
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+
 }
+
+
+
