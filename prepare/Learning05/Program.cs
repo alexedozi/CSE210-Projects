c381264e3c2037b5e96cf5647bdebf61f3128a3f
alexedozi <chukwujioke60@gmail.com>
2023-06-12 22:10:12 +0200

    ShapeActivity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 3, 4));
+        shapes.Add(new Circle("Green", 2));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: " + shape.Color);
+            Console.WriteLine("Area: " + shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Shape
+{
+    public string Color { get; set; }
+
+    public Shape(string color)
+    {
+        Color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
